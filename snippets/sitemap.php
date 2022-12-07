a3bf4904ae59baeb25a41f4868b7ca4cb00d1d06
Marvin Siefke <119980601+marvinsiefke@users.noreply.github.com>
2022-12-07 16:29:12 +0100

    Update sitemap.php

--- snippets/sitemap.php
+++ snippets/sitemap.php
@@ -1,10 +1,10 @@
 <?= '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-	<?php foreach ($pages as $p): ?>
+	<?php foreach ($pages as $page): ?>
 		<url>
-			<loc><?= html($p->url()) ?></loc>
-			<lastmod><?= $p->modified('c', 'date') ?></lastmod>
-			<priority><?= ($p->isHomePage()) ? 1 : number_format(0.5 / $p->depth(), 1) ?></priority>
+			<loc><?= html($page->url()) ?></loc>
+			<lastmod><?= $page->modified('c', 'date') ?></lastmod>
+			<priority><?= ($page->isHomePage()) ? 1 : number_format(0.5 / $page->depth(), 1) ?></priority>
 		</url>
 	<?php endforeach ?>
 </urlset>
